5e3bb4a4f0fdd9e638a410d02e198bb133e40d95
Emmanuel Valenzuela <ing.valen0421@hotmail.com>
2021-10-31 22:36:40 -0400

    uploading changes for iphone

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -5,25 +5,43 @@ import IFloatingActionButton from '../model/interfaces/IFloatingActionButton';
 const FloatingActionButton = ({ title, onPress, position = 'bottomLeft' }: IFloatingActionButton) => {
 
 
-    return (
-
-        <View
-            style={[styles.locationFAB,
-            (position === 'bottomLeft') ? styles.leftSide : styles.rightSide
-            ]}
-        >
-            <TouchableNativeFeedback
+    const ios = () => {
+        return (
+            <TouchableOpacity
+                style={[styles.locationFAB,
+                (position === 'bottomLeft') ? styles.leftSide : styles.rightSide
+                ]}
+                activeOpacity={0.7}
                 onPress={onPress}
-                background={ TouchableNativeFeedback.Ripple('#28425B', false, 40) }
             >
                 <View style={styles.botonFAB} >
                     <Text style={styles.textFAB} >{title}</Text>
                 </View>
-            </TouchableNativeFeedback>
+            </TouchableOpacity>
+        )
+    }
 
-        </View>
+    const android = () => {
+        return (
+            <View
+                style={[styles.locationFAB,
+                (position === 'bottomLeft') ? styles.leftSide : styles.rightSide
+                ]}
+            >
+                <TouchableNativeFeedback
+                    background={TouchableNativeFeedback.Ripple('#28425B', false, 40)}
+                    onPress={onPress}
+                >
+                    <View style={styles.botonFAB} >
+                        <Text style={styles.textFAB} >{title}</Text>
+                    </View>
+                </TouchableNativeFeedback>
 
-    )
+            </View>
+        )
+    }
+
+    return (Platform.OS === 'android') ? ios() : android()
 }
 
 export default FloatingActionButton
@@ -32,7 +50,16 @@ const styles = StyleSheet.create({
 
     locationFAB: {
         position: 'absolute',
-        bottom: 49
+        bottom: 49,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 9,
     },
     rightSide: {
         right: 25
@@ -46,15 +73,6 @@ const styles = StyleSheet.create({
         height: 60,
         borderRadius: 100,
         justifyContent: 'center',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.32,
-        shadowRadius: 5.46,
-
-        elevation: 9,
     },
     textFAB: {
         color: 'white',
