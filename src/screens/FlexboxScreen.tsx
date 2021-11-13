5c1a8e591f53c44d94b31974fa73a1352acbe057
Emmanuel Valenzuela <ing.valen0421@hotmail.com>
2021-11-13 10:46:07 -0400

    Subiendo cambios correspondientes a flex

--- src/screens/FlexboxScreen.tsx
+++ src/screens/FlexboxScreen.tsx
@@ -3,12 +3,40 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const FlexboxScreen = () => {
     return (
-        <View>
-            <Text>gkgkgkhjg</Text>
+        <View style={styles.container} >
+            <Text style={styles.caja1} >Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
         </View>
     )
 }
 
 export default FlexboxScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F0A23B'
+    },
+    caja1: {
+        flex: 4,
+        borderWidth: 2,
+        borderColor: 'white',
+        fontSize: 30
+    },
+    caja2: {
+        flex: 4,
+        borderWidth: 2,
+        borderColor: 'white',
+        fontSize: 30
+    },
+    caja3: {
+        flex: 2,
+        borderWidth: 2,
+        borderColor: 'white',
+        fontSize: 30
+    }
+
+})
